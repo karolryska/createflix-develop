61fc5bfc3b2da0f20f0233b3e8933a949852625e
Karol Ryska <karolryska@gmail.com>
2023-02-28 20:16:45 +0100

    add isFavorite prop to movie list item component

--- src/elements/components/movies-list/MoviesList.tsx
+++ src/elements/components/movies-list/MoviesList.tsx
@@ -1,17 +1,24 @@
 import MovieListItem from '../movie-list-item/MovieListItem';
 import { MovieListItem as MovieListItemProps } from '../../../types';
+import { useFavorites } from '../../../store';
 
 type MovieListProps = {
     data: MovieListItemProps[];
 };
 
 export default function MoviesList({ data }: MovieListProps) {
+    const { favorites } = useFavorites();
+
     return (
         <ul className={['flex', 'flex-col'].join(' ')}>
             {!!data.length &&
                 data.map((movie) => (
                     <li className={['flex', 'font-sans'].join(' ')}>
-                        <MovieListItem data={movie} />
+                        <MovieListItem
+                            key={movie.id}
+                            data={movie}
+                            isFavorite={!!favorites.find(({ id }) => id === movie.id)}
+                        />
                     </li>
                 ))}
         </ul>
